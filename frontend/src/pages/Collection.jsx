2e207a5bb0727137da3409fce16961c2252a4a0d
anshumand4 <adiwakar19@gmail.com>
2025-07-06 23:14:47 +0530

    ain

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -101,7 +101,7 @@ const Collection = () => {
               <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory}/> Women
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Unisex'} onChange={toggleCategory}/> Unisex
+              <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/> kids
             </p>
           </div>
         </div>
@@ -110,19 +110,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Backpacks'} onChange={toggleSubCategory}/> Backpacks
+              <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/> Topwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Moneybelt'} onChange={toggleSubCategory}/> Moneybelt
+              <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/> Bottomwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Sideslings'} onChange={toggleSubCategory}/> Sidesliding
-            </p>
-            <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Cluthers'} onChange={toggleSubCategory}/> Cluthers
-            </p>
-            <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Misc'} onChange={toggleSubCategory}/> Misc
+              <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/> Winterwear
             </p>
           </div>
         </div>
